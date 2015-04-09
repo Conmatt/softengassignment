d8c4c578f1ca3ac7864ec34d76b7b744c4d2634f
Slepnir <connorattard@gmail.com>
2015-04-09 17:46:16 +0200

    Test
    Test

---
+++ src/Tester.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Connor on 09/04/15.
+ */
+public class Tester {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World! It's Matt and Connor");
+    }
+}
