fbd72ec516d4571b0a87a47fca3a4735af4d8235
Slepnir <connorattard@gmail.com>
2015-04-21 23:27:45 +0200

    Test#3
    Testing that null is returned if an inexistent account number is given.

--- src/test/java/um/edu/mt/TestSuite.java
+++ src/test/java/um/edu/mt/TestSuite.java
@@ -10,6 +10,7 @@ public class TestSuite {
 	Account acc2 = new Account(0, "John Doe", 500);
 	Account acc3 = new Account(1, "Jane Doe", 10000); 
 	
+	//Adding an account and checking its fields
 	@Test
 	public void addAccount() {
 		boolean ret = AccountDatabase.addAccount(acc1);
@@ -23,6 +24,7 @@ public class TestSuite {
 		Assert.assertEquals(ret, true);
 	}
 	
+	//Forcing key collision in the hash table
 	@Test
 	public void keyCollision() {
 		boolean ret = AccountDatabase.addAccount(acc2);
@@ -30,6 +32,7 @@ public class TestSuite {
 		Assert.assertEquals(ret, false);
 	}
 	
+	//Adding a second account
 	@Test
 	public void addSecondAccount() {
 		boolean ret = AccountDatabase.addAccount(acc3);
@@ -41,5 +44,13 @@ public class TestSuite {
 		Assert.assertEquals(test.getBalance(), 10000);
 		
 		Assert.assertEquals(ret, true);
+	}
+	
+	//Retrieving an inexistent account number
+	@Test
+	public void getInvalidNumber() {
+		Account ret = AccountDatabase.getAccount(70);
+		
+		Assert.assertEquals(ret, null);
 	}
 }
